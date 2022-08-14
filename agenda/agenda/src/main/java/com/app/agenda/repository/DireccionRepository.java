4bcc4d46930eb9462e3b02d7cf1eafdfffdb722d
SergioQC1 <111206933+SergioQC1@users.noreply.github.com>
2022-08-13 20:17:54 -0600

    Update DireccionRepository.java

--- agenda/agenda/src/main/java/com/app/agenda/repository/DireccionRepository.java
+++ agenda/agenda/src/main/java/com/app/agenda/repository/DireccionRepository.java
@@ -1,4 +1,11 @@
 package com.app.agenda.repository;
 
-public class DireccionRepository {
+import com.app.agenda.entity.Direccion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.io.Serializable;
+
+@Repository("direccionRepository")
+public interface DireccionRepository extends JpaRepository<Direccion, Serializable> {
 }
