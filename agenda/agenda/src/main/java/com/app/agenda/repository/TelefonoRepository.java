0402e868907435749cdc967bf39d85892ed2fec2
SergioQC1 <111206933+SergioQC1@users.noreply.github.com>
2022-08-13 20:19:13 -0600

    Update TelefonoRepository.java

--- agenda/agenda/src/main/java/com/app/agenda/repository/TelefonoRepository.java
+++ agenda/agenda/src/main/java/com/app/agenda/repository/TelefonoRepository.java
@@ -1,4 +1,11 @@
 package com.app.agenda.repository;
 
-public class TelefonoRepository {
+import com.app.agenda.entity.Telefono;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.io.Serializable;
+
+@Repository("telefonoRepository")
+public interface TelefonoRepository extends JpaRepository<Telefono, Serializable> {
 }
