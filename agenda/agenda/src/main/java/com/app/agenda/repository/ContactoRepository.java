3dc206187edf23a6a50a4937fc52951340fc5b37
SergioQC1 <111206933+SergioQC1@users.noreply.github.com>
2022-08-13 20:16:28 -0600

    Update ContactoRepository.java

--- agenda/agenda/src/main/java/com/app/agenda/repository/ContactoRepository.java
+++ agenda/agenda/src/main/java/com/app/agenda/repository/ContactoRepository.java
@@ -1,4 +1,15 @@
 package com.app.agenda.repository;
 
-public class ContactoRepository {
+import com.app.agenda.entity.Contacto;
+import com.app.agenda.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Repository("contactoRepository")
+public interface ContactoRepository extends JpaRepository<Contacto, Serializable> {
+
+    public List<Contacto> findByUsuarioCorreo(String usuarioCorreo);
 }
