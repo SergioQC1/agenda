c26f02a0380eab52bbd0cfd48b14b3a55819a06d
SergioQC1 <111206933+SergioQC1@users.noreply.github.com>
2022-08-13 20:21:22 -0600

    Update RolService.java

--- agenda/agenda/src/main/java/com/app/agenda/service/RolService.java
+++ agenda/agenda/src/main/java/com/app/agenda/service/RolService.java
@@ -1,4 +1,27 @@
 package com.app.agenda.service;
 
+import com.app.agenda.entity.Rol;
+import com.app.agenda.repository.RolRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rol")
+@CrossOrigin
 public class RolService {
+
+    @Autowired
+    RolRepository rolRepository;
+
+    @GetMapping( path = "/buscar" )
+    public List<Rol> getAllRol(){
+        return rolRepository.findAll();
+    }
+
+    @PostMapping( path = "/guardar" )
+    public Rol saveRol(@RequestBody Rol rol){
+        return rolRepository.save(rol);
+    }
 }
