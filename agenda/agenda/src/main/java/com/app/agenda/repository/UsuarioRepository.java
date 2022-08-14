89b89c99921e4990c1b704d6d3f44433f35f5917
SergioQC1 <111206933+SergioQC1@users.noreply.github.com>
2022-08-13 20:19:59 -0600

    Update UsuarioRepository.java

--- agenda/agenda/src/main/java/com/app/agenda/repository/UsuarioRepository.java
+++ agenda/agenda/src/main/java/com/app/agenda/repository/UsuarioRepository.java
@@ -1,4 +1,16 @@
 package com.app.agenda.repository;
 
-public class UsuarioRepository {
+
+import com.app.agenda.entity.Rol;
+import com.app.agenda.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Repository("usuarioRepository")
+public interface UsuarioRepository extends JpaRepository<Usuario, Serializable> {
+
+    public List<Usuario> findByCorreoAndPassword(String correo, String password);
 }
